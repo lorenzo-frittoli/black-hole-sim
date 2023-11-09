98c428ada47e0a36f637115679f916224f8dcfdc
lorenzo-frittoli <lorenzo.frittoli.ge@gmail.com>
2023-11-09 08:07:47 +0100

    Removed initial parameters

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from simulator import Simulator
 
 def main():
-    sim = Simulator(
-        mass = 1,
-        energy = 2,
-        angular_momentum = 15,
-        starting_distance = 30,
-        starting_angle = 3.14,
-        starting_speed = 0
-    )
+    sim = Simulator()
     
     sim.simulate()
-    sim.show(time_scale = 10, do_daddy_pig=True, do_animation=False)
+    sim.show()
 
 
 if __name__ == '__main__':
